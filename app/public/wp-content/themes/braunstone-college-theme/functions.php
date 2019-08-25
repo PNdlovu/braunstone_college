71973548d3c6eb2de07a124afd3dabad676669a5
phil <pitso@live.co.uk>
2019-08-26 00:38:30 +0100

    Convert static html template

--- app/public/wp-content/themes/braunstone-college-theme/functions.php
+++ app/public/wp-content/themes/braunstone-college-theme/functions.php
@@ -1,9 +1,9 @@
 <?php 
   
 function braunstone_files() {
-	wp_enqueue_style('google fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i')
-	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')
-	wp_enqueue_style('braunstone__main_styles' , get_stylesheet_uri());
+	wp_enqueue_style('google fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	wp_enqueue_style('braunstone__main_styles' , get_stylesheet_uri(), NULL,microtime());
 }
 
 
